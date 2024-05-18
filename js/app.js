a1a47a2e9a101ec31c214e71a4b04f59d38a0f81
Muhammad Naveed <85978999+uxlabspk@users.noreply.github.com>
2024-05-18 17:53:01 +0500

    Modifying code for production grade

--- js/app.js
+++ js/app.js
@@ -2,18 +2,17 @@
 
 // © 2023 CodeHUNT'S, Inc. · <a href="./terms.html" target="_blank" class="text-light">Terms.</a>
 
-document.getElementById('year').innerText = `© ` + new Date().getFullYear() + " CodeHUNT'S, Inc. · ";
+document.getElementById('year').innerText = `© 2023 - ` + new Date().getFullYear() + " CodeHUNT'S · "
+// (new Date().getFullYear() == '2024') ? `© ` + new Date().getFullYear() + " CodeHUNT'S, Inc. · " : `© 2024 - ` + new Date().getFullYear() + " CodeHUNT'S, Inc. · ";
 
 let backtotop = document.getElementById('backtotop');
 if (backtotop) {
     const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
+        if (window.scrollY > 100) 
             backtotop.classList.add('active')
-        } else {
+        else 
             backtotop.classList.remove('active')
-        }
     }
     window.addEventListener('load', toggleBacktotop)
     document.addEventListener('scroll', toggleBacktotop)
-    // onscroll(document, toggleBacktotop)
 }
