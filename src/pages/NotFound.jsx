2fb06f32d1dd6d8606f076fce4755f661344d80a
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-06-10 21:19:19 +0500

    Adding Not Found Page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,24 @@
+import NavBar from "../components/NavBar.jsx";
+import Footer from "../components/Footer.jsx";
+import NotFoundImg from "../assets/error.png";
+import FilledButton from "../components/Buttons/FilledButton.jsx";
+
+export default function NotFound() {
+    return (
+       <>
+           {/* NavBar */}
+           <NavBar />
+
+           {/* Not Found */}
+           <section className="relative h-screen flex flex-col items-center justify-center">
+               <img src={NotFoundImg} alt="404" />
+               <h1 className=" text-gray-900 text-3xl font-semibold mt-4">404 | Page Not Found</h1>
+               <p className="mt-4">The specified file was not found on this website. Please check the URL for mistakes and try again.</p>
+               <FilledButton to={"/"} text={"Go Home"} style={"px-12 py-3 mt-8"} />
+           </section>
+
+           {/* Footer */}
+           <Footer />
+       </>
+    )
+}
