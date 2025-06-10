d4e34ce58b38757777c10bc8e43e2e4099d8d90f
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-06-10 19:03:04 +0500

    Updating Hero Section

--- src/components/Buttons/FilledButton.jsx
+++ src/components/Buttons/FilledButton.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 export default function FilledButton({ text, to, style = "" }) {
     return (
-        <Link to={to} className={`bg-amber-800 text-white  rounded-full hover:bg-amber-900 transition ${style}`}>
+        <Link to={to} className={`${style} bg-amber-800 text-white  rounded-full hover:bg-amber-900 transition`}>
             {text}
         </Link>
     )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,17 @@
 import FilledButton from "./Buttons/FilledButton.jsx";
-import OutlineButton from "./Buttons/OutlineButton.jsx";
 
 
 export default function Hero() {
     return (
-        <section id="home" className="bg-gray-50 sm:h-[90vh] h-[80vh] flex items-center justify-center">
+        <section id="home" className="relative bg-gray-50 sm:h-[90vh] h-[80vh] flex items-center justify-center overflow-hidden">
+            {/* Adding Background Animation */}
+            <div className="absolute top-1/4 right-0 w-64 h-64 bg-green-300 rounded-full mix-blend-multiply blur-2xl opacity-30 blob-animate z-0"></div>
+            <div className="absolute bottom-1/4 left-0 w-80 h-80 bg-blue-300 rounded-full mix-blend-multiply blur-2xl opacity-30 blob-animate z-0"></div>
+
+            <div className="absolute top-1/2 left-1/4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply blur-2xl opacity-30 blob-animate" style={{ animationDuration: '10s' }}></div>
+            <div className="absolute bottom-1/2 right-1/4 w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply blur-2xl opacity-20 blob-animate" style={{ animationDuration: '15s' }}></div>
+
+            {/* Content */}
             <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
                 <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-gray-900 mb-6 leading-tight">
                     Innovative Software Solutions for Your Business
@@ -23,15 +30,9 @@ export default function Hero() {
                         rel="noopener noreferrer"
                         className="inline-flex underline items-center text-sm text-gray-600 hover:text-green-600 transition-colors"
                     >
-                        {/*<img*/}
-                        {/*    src="https://cdn.trustpilot.net/brand-assets/4.4.0/stars/stars-4.svg"*/}
-                        {/*    alt="Trustpilot rating"*/}
-                        {/*    className="w-24 h-auto mr-2"*/}
-                        {/*/>*/}
                         Review us on Trustpilot
                     </a>
                 </div>
-
             </div>
         </section>
 
