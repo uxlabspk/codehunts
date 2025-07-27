e40231e19661504d9693b16c531d569a7b27b550
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-07-27 19:21:38 +0500

    feat: Adding Required Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
+import {BrowserRouter, Routes, Route} from "react-router";
+import Home from "@/pages/landing.tsx";
+import Portfolio from "@/pages/portfolio.tsx";
+import Team from "@/pages/our-team.tsx";
+import About from "@/pages/about.tsx";
+import Contact from "@/pages/contact-us.tsx";
+import Talk from "@/pages/talk.tsx";
+import Terms from "@/pages/terms-conditions.tsx";
+import NotFound from "@/pages/not-found.tsx";
+import WebDev from "@/pages/web-development.tsx";
+import AppDev from "@/pages/app-development.tsx";
+import Graphics from "@/pages/graphics-designing.tsx";
+import AI from "@/pages/ai-development.tsx";
+import CloudSolutions from "@/pages/cloud-solutions.tsx";
+import SoftwareConsulting from "@/pages/custom-software.tsx";
+import Header from "@/components/common/header.tsx";
 
 
 function App() {
 
   return (
-    <>
-      <p className={'bg-gray-100 text-8xl'}>testing</p>
-    </>
+      <BrowserRouter>
+          <Header />
+          <Routes>
+              <Route path={'/'} element={<Home />} />
+              <Route path={'/web-development'} element={<WebDev />} />
+              <Route path={'/app-development'} element={<AppDev />} />
+              <Route path={'/ai-development'} element={<AI />} />
+              <Route path={'/graphics-designing'} element={<Graphics />} />
+              <Route path={'/cloud-solutions'} element={<CloudSolutions />} />
+              <Route path={'/custom-software'} element={<SoftwareConsulting />} />
+              <Route path={'/portfolio'} element={<Portfolio />} />
+              <Route path={'/our-team'} element={<Team />} />
+              <Route path={'/about-us'} element={<About />} />
+              <Route path={'/contact-us'} element={<Contact />} />
+              <Route path={'/lets-talk'} element={<Talk />} />
+              <Route path={'/terms'} element={<Terms />} />
+              <Route path={'/*'} element={<NotFound />} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
