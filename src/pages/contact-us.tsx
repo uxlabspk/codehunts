e1a41bfbf979ba1a185272f46e658a95bd4ef580
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-07-31 22:41:53 +0500

    feat: Updating Contact Us Page

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/common/hero-section.tsx";
 import CompanyStats from "@/components/common/company-stats.tsx";
+import ContactSection from "@/components/common/contact-section.tsx";
+import CTASection from "@/components/common/cta-section.tsx";
 
 export default function Contact() {
   return (
     <>
       <HeroSection title={'Lets Build Something Amazing Together'} description={'Ready to transform your ideas into powerful software solutions? Our expert team is here to bring your vision to life.'} hasLinks={false} />
       <CompanyStats />
+      <ContactSection />
+      <CTASection />
     </>
   )
 }
