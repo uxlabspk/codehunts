80689336040a5271bf8190124b8a3a72335e9e60
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2025-07-31 22:45:51 +0500

    feat: Updating About Us Page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,23 @@
-
+import HeroSection from "@/components/common/hero-section.tsx";
+import CompanyStats from "@/components/common/company-stats.tsx";
+import OurValues from "@/components/common/our-values.tsx";
+import TeamSection from "@/components/landing/team-section.tsx";
+import CTASection from "@/components/common/cta-section.tsx";
 
 
 export default function About() {
     return(
-        <p>
-            about
-        </p>
+        <>
+            <HeroSection
+                title={'About Code HUNT\'S'}
+                description={'We\'re a passionate team of developers, designers, and innovators crafting digital experiences that transform businesses and delight users.'}
+                hasLinks={false} />
+
+            <CompanyStats />
+            {/* Our Story */}
+            <TeamSection />
+            <OurValues />
+            <CTASection />
+        </>
     )
 }
